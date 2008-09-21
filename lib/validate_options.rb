407c2a74e8b9c4be0a4743a15a570f7c335fc207
Damian Janowski <damian.janowski@gmail.com>
2008-09-21 03:13:02 -0300

    Changing strategy: defining the utility method in Kernel rather than extending Object.

--- lib/validate_options.rb
+++ lib/validate_options.rb
@@ -1,27 +1,27 @@
 module ValidateOptions
   def self.disable!
-    undef_method :validate_options if method_defined?(:validate_options)
-
-    def validate_options(hash, *keys)
-      # no-op
+    Kernel.module_eval do
+      def validate_options(hash, *keys)
+        # no-op
+      end
     end
   end
 
   def self.enable!
-    undef_method :validate_options if method_defined?(:validate_options)
+    Kernel.module_eval do
+      def validate_options(hash, *keys)
+        return unless hash
+        extra = hash.keys - keys
+        raise ArgumentError, "Invalid options provided: #{extra.map {|k| k.inspect }.join(', ')}." unless extra.empty?
 
-    def validate_options(hash, *keys)
-      return unless hash
-      extra = hash.keys - keys
-      raise ArgumentError, "Invalid options provided: #{extra.map {|k| k.inspect }.join(', ')}." unless extra.empty?
-
-      if block_given?
-        begin
-          yield(hash)
-        rescue ArgumentError
-          raise
-        rescue Exception => e
-          raise ArgumentError, e
+        if block_given?
+          begin
+            yield(hash)
+          rescue ArgumentError
+            raise
+          rescue Exception => e
+            raise ArgumentError, e
+          end
         end
       end
     end
@@ -46,4 +46,4 @@ module ValidateOptions
   disable!
 end
 
-Object.send(:include, ValidateOptions)
+Class.send(:include, ValidateOptions)
